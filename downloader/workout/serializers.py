c47bea46a299c0f8c950f24f8ffd0e7d9108a0f5
Chris Champa <chrismchampa@gmail.com>
2021-07-12 15:56:40 -0400

    Refactoring to accept nested workout creation

--- downloader/workout/serializers.py
+++ downloader/workout/serializers.py
@@ -27,30 +27,8 @@ class MovementClassSerializer(serializers.ModelSerializer):
         model = MovementClass
         fields = '__all__'
 
-class WorkoutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workout
-        fields = '__all__'
-
-    def validate(self, data):
-        if (data['complete']) and (data['date'] > date.today()):
-            return serializers.ValidationError('Workout cannot be completed before it is listed as occurring')
-        return super().validate(data)
-
-class SectionSerializer(serializers.ModelSerializer):
-
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        if ret['score_number']:
-            ret['score_number'] = ret['score_number'].strip('0').strip('.')
-        return ret
-
-    class Meta:
-        model = Section
-        fields = '__all__'
-
 class MovementInstanceSerializer(serializers.ModelSerializer):
-
+    id = serializers.IntegerField(required=False)
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         print(ret)
@@ -63,6 +41,7 @@ class MovementInstanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovementInstance
         fields = '__all__'
+        read_only_fields = ('workout_id', 'section_id', 'user')
 
     def validate(self, data):
         ThisMovement = MovementClass.objects.get(name=data['movement_id'])
@@ -70,6 +49,103 @@ class MovementInstanceSerializer(serializers.ModelSerializer):
             return serializers.ValidationError('Cannot select unsupported count type for this movement')
         if data['score_type'] not in ThisMovement.score_types.all():
             return serializers.ValidationError('Cannot select unsupported score type for this movement')
-        if data['section_id'].workout_id != data['workout_id']:
-            return serializers.ValidationError('Movement cannot belong to a section that does not correspond to the same workout as the movement')
+        return super().validate(data)
+
+class SectionSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if ret['score_number']:
+            ret['score_number'] = ret['score_number'].strip('0').strip('.')
+        return ret
+
+    movementinstance_set = MovementInstanceSerializer(many=True)
+    class Meta:
+        model = Section
+        fields = '__all__'
+        read_only_fields = ('id', 'workout_id', 'user')
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    section_set = SectionSerializer(many=True)
+    class Meta:
+        model = Workout
+        fields = '__all__'
+
+    def create(self, validated_data):
+        sections = validated_data.pop('section_set')
+        workout = Workout.objects.create(**validated_data)
+        for section in sections:
+            movements = section.pop('movementinstance_set')
+            section.pop('id', None)
+            section_instance = Section.objects.create(
+                        **section,
+                        workout_id = workout,
+                        user = workout.user
+                        )
+            for movement in movements:
+                MovementInstance.objects.create(
+                    **movement,
+                    section_id = section_instance,
+                    workout_id = workout,
+                    user = workout.user
+                )
+        return workout
+
+    def update(self, instance, validated_data):
+        sections = validated_data.pop('section_set')
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        keep_sections = []
+        for section in sections:
+            id = section.pop('id', None)
+            movements = section.pop('movementinstance_set', None)
+            if id:
+                if Section.objects.filter(id=id).exists():
+                    section_instance = Section.objects.get(id=id)
+                    for key, value in section.items():
+                        setattr(section_instance, key, value)
+                    section_instance.save()
+                    keep_sections.append(id)
+            else:
+                section_instance = Section.objects.create(
+                    **section,
+                    workout_id=instance,
+                    user = instance.user
+                    )
+                keep_sections.append(section_instance.id)
+
+            keep_movements = []
+            for movement in movements:
+                movement_id = movement.pop('id', None)
+                if movement_id:
+                    if MovementInstance.objects.filter(id=movement_id).exists():
+                        movement_instance = MovementInstance.objects.get(id=movement_id)
+                        for key, value in movement.items():
+                            setattr(movement_instance, key, value)
+                        movement_instance.save()
+                        keep_movements.append(movement_id)
+                else:
+                    movement_instance = MovementInstance.objects.create(
+                        **movement,
+                        workout_id = instance,
+                        section_id = section_instance,
+                        user = instance.user
+                        )
+                    keep_movements.append(movement_instance.id)
+            for movement in section_instance.movementinstance_set.all():
+                if movement.id not in keep_movements:
+                    movement.delete()
+
+        for section in instance.section_set.all():
+            if section.id not in keep_sections:
+                section.delete()
+
+        return instance
+
+
+
+    def validate(self, data):
+        if (data['complete']) and (data['date'] > date.today()):
+            return serializers.ValidationError('Workout cannot be completed before it is listed as occurring')
         return super().validate(data)
