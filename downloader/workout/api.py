2cca5614091dcd6fda075197cee0a2359893a396
Chris Champa <chrismchampa@gmail.com>
2021-07-16 15:05:50 -0400

    Moving results explorer API to movement class actions

--- downloader/workout/api.py
+++ downloader/workout/api.py
@@ -1,13 +1,14 @@
-from .models import MovementClass, Workout, Section, MovementInstance
+from .models import MovementClass, Workout, Section, MovementInstance, ScoreTypes, CountTypes
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from .serializers import WorkoutSerializer, SectionSerializer, MovementClassSerializer, MovementInstanceSerializer, LoginSerializer, WhoopiePieUserSerializer
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .models import WhoopiePieUser
 from django.db.models.aggregates import Max, Min
-from django.db.models import Q
+from django.db.models import Q, F
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginSerializer
@@ -86,6 +87,44 @@ class MovementClassViewSet(ModelViewSet):
         'name':['icontains', 'exact']
     }
 
+    @action(detail=True)
+    def unique_counts(self, request, pk=None):
+        movement = self.get_object()
+        movement_instances = movement.movementinstance_set\
+                              .filter(user=request.user.whoopiepieuser)\
+                              .exclude(Q(score_time='00:00:00') & Q(score_number=None))\
+                              .values('count', 'count_type', 'score_type').distinct()
+        return Response(movement_instances, status=status.HTTP_200_OK)
+
+    @action(detail=True)
+    def max_score(self, request, pk=None):
+        movement = self.get_object()
+        movement_instances = movement.movementinstance_set.filter(user=request.user.whoopiepieuser)
+        best_lbs = movement_instances.exclude(score_number=None)\
+                    .filter(score_type=ScoreTypes('lbs'))\
+                    .values('count_type', 'count')\
+                    .annotate(score_number=Max('score_number'))
+        best_time = movement_instances.exclude(score_time='00:00:00')\
+                    .filter(score_type=ScoreTypes('time'))\
+                    .values('count_type', 'count')\
+                    .annotate(score_time=Min('score_time'))
+        return Response({
+            'lbs':best_lbs,
+            'time':best_time,
+        }, status=status.HTTP_200_OK)
+
+    @action(detail=True)
+    def progression(self, request, pk=None):
+        movement = self.get_object()
+        movement_instances = movement.movementinstance_set\
+                .filter(user=request.user.whoopiepieuser)\
+                .exclude(Q(score_number=None) & Q(score_time='00:00:00'))\
+                .annotate(date=F('workout_id__date'))\
+                .values('date', 'count_type', 'count')\
+                .annotate(score_number = Max('score_number'),
+                          score_time = Min('score_time'))
+        return Response(movement_instances, status=status.HTTP_200_OK)
+
 
 class MovementInstanceViewSet(ModelViewSet):
     serializer_class = MovementInstanceSerializer
@@ -109,23 +148,3 @@ class MovementInstanceViewSet(ModelViewSet):
     def update(self, request, *args, **kwargs):
         request.data['user']=request.user.id
         return super().update(request, *args, **kwargs)
-
-    def list(self, request, *args, **kwargs):
-        unique_counts = request.query_params.get('unique_counts', None)
-        movement = request.query_params.get('movement_id', None)
-        max_score = request.query_params.get('max_score', None)
-        if unique_counts:
-            if not movement:
-                return Response({'error':'You must specify a movement in order to get the unique movement counts'}, status=status.HTTP_400_BAD_REQUEST)
-            data = MovementInstance.objects.filter(movement_id=movement).values('count', 'count_type').distinct()
-            return Response(data, status=status.HTTP_200_OK)
-        if max_score:
-            if not movement:
-                return Response({'error':'You must specify a movement in order to get max scores'}, status=status.HTTP_400_BAD_REQUEST)
-            data = MovementInstance.objects.filter(movement_id=movement)\
-                    .exclude(Q(score_number=None) & Q(score_time='00:00:00'))\
-                    .values('score_type', 'count_type', 'count')\
-                    .annotate(score_number=Max('score_number'),
-                              score_time=Min('score_time'))
-            return Response(data, status=status.HTTP_200_OK)
-        return super().list(request, *args, **kwargs)
