1f5b5747b03980cb6c19a2eb775fffc8e48b4193
Chris Champa <chrismchampa@gmail.com>
2021-12-19 10:09:37 -0500

    adding script name to static url

--- downloader/downloader/settings.py
+++ downloader/downloader/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = FORCE_SCRIPT_NAME+'/static/'
 
 WEBPACK_LOADER = {
     'DEFAULT': {
